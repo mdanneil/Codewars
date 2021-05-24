f2b64712a56082ccecb655fae0e884a32a9b1b4a
Danneil <ddewee92@gmail.com>
2021-05-24 22:44:56 +0200

    CountingSheep kata

---
+++ src/test/java/Kyu8/CountingSheepTest.java
@@ -0,0 +1,20 @@
+package Kyu8;
+
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class CountingSheepTest {
+    Boolean[] array1 = {true,  true,  true,  false,
+            true,  true,  true,  true ,
+            true,  false, true,  false,
+            true,  false, false, true ,
+            true,  true,  true,  true ,
+            false, false, true,  true };
+
+    @Test
+    public void test() {
+        assertEquals("There are 17 sheeps in total", 17, new CountingSheep().countSheeps(array1));
+    }
+}
