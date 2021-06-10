7137ce14931e6227eb2faa206cbc5ecb4a50b4b2
Danneil <ddewee92@gmail.com>
2021-06-10 22:47:37 +0200

    ReturnNegative kata

---
+++ src/main/java/Kyu8/ReturnNegative.java
@@ -0,0 +1,7 @@
+package Kyu8;
+
+public class ReturnNegative {
+    public static int makeNegative(final int x) {
+        return x > 0 ? -x : x;
+    }
+}
---
+++ src/test/java/Kyu8/ReturnNegativeTest.java
@@ -0,0 +1,12 @@
+package Kyu8;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class ReturnNegativeTest {
+    @Test
+    public void test1() {
+        assertEquals(-42, ReturnNegative.makeNegative(42));
+    }
+}
